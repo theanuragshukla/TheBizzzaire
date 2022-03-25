c90071368b95cd238a2f1482186cde934a11a400
zaidazaize <zaid.azaize@gmail.com>
2022-03-25 12:34:15 +0530

    screen capture and view stream

---
+++ public/static/js/showdata.js
@@ -0,0 +1,21 @@
+function startStream(){
+
+}
+async function startCapture(displayMediaOptions) {
+    let captureStream = null;
+  
+    try {
+      captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    } catch(err) {
+      console.error("Error: " + err);
+    }
+    return captureStream;
+  }
+  function destroyViews(){
+      document.getElementById("values").remove();
+      document.getElementById("streamButton").remove();
+      var videoele = document.createElement("video");
+      videoele.setAttribute("src",sessionStorage.getItem("streamUrl"))
+  }
+
+  
