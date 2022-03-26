b5bb094c9fcb540e07ecbd75de6bf10c53210ac9
Aditya <adityathebest2019@gmail.com>
2022-03-26 12:35:31 +0530

    initial css

--- public/static/js/script.js
+++ public/static/js/script.js
@@ -1,7 +1,12 @@
 //const web3 = new Web3("https://cloudflare-eth.com")
 const btns = document.getElementById("btns"); 
+const welcome = document.querySelector(".welcome"); 
 const watchDiv = document.getElementById("watch"); 
 const inp = document.getElementById("urlInp"); 
+const view = document.querySelector(".super");
+const card1 = document.querySelector(".card1");
+const card2 = document.querySelector(".card2");
+
 
 
 function create(){
@@ -14,9 +19,26 @@ function create(){
 }
 
 function triggerWatch(){
-	btns.style.display="none";	
+	// btns.style.display="none";	
+	welcome.style.display="none";	
 	watchDiv.style.display="flex";
+	card1.style.display="none";
+	card2.style.display="none";
+
 }
+function goBack(){
+	// btns.style.display="flex";	
+	welcome.style.display="flex";	
+	watchDiv.style.display="none";
+	view.style.height="200vh"
+	card1.style.display="flex";
+	card2.style.display="flex";
+}
+
+function superHeight(){
+	view.style.height="100vh";
+}
+
 function watch (){
 	var streamUrl=inp.value;
 	sessionStorage.setItem("streamUrl",streamUrl);
