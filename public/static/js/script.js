29f732ed51fa4b261523697d5f2205d7c1a751d6
Aditya <adityathebest2019@gmail.com>
2022-03-29 09:13:24 +0530

    Full responsive index.html

--- public/static/js/script.js
+++ public/static/js/script.js
@@ -3,9 +3,9 @@ const btns = document.getElementById("btns");
 const welcome = document.querySelector(".welcome"); 
 const watchDiv = document.getElementById("watch"); 
 const inp = document.getElementById("urlInp"); 
-const view = document.querySelector(".super");
 const card1 = document.querySelector(".card1");
 const card2 = document.querySelector(".card2");
+const back = document.querySelector(".back");
 
 function create(){
 	location.href="/create";
@@ -17,20 +17,18 @@ function triggerWatch(){
 	watchDiv.style.display="flex";
 	card1.style.display="none";
 	card2.style.display="none";
+	back.style.display="flex";
 
 }
 function goBack(){
 	// btns.style.display="flex";	
 	welcome.style.display="flex";	
 	watchDiv.style.display="none";
-	view.style.height="200vh"
 	card1.style.display="flex";
 	card2.style.display="flex";
+	back.style.display="none";
 }
 
-function superHeight(){
-	view.style.height="100vh";
-}
 
 function watch (){
 		var streamUrl=inp.value;
