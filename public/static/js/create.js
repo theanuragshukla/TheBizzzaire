266a6bf9003a9b75b8bb7fc17a6a7a7ae4aad670
Anurag Shukla <tachyonanurag@gmail.com>
2022-03-30 14:22:26 +0530

    improved security and added logs

--- public/static/js/create.js
+++ public/static/js/create.js
@@ -1,71 +1,41 @@
-const RATE=0.008;
+const RATE = 0.008;
 const logs = document.getElementById('logs');
+const transaction = document.querySelector('.transaction');
 var usdToEther;
-		async	function setVal(e){
-			document.getElementById("cost").innerHTML=RATE*e.value;
-			document.getElementById("streamDur").innerHTML=e.value;
-		}
-
-			async function submit(){
-
-				var name = document.getElementById("name").value;
-				sessionStorage.setItem('streamName',name);
-				var dur =0;
-				dur = document.getElementById("duration").value;
-				await fetch('/create',
-					{
-						method: 'POST',
-						headers: {
-							'Content-Type': 'application/json'
-						},
-						body: JSON.stringify({
-							data: {
-								name: name,
-								dur: dur
-							}
-						})
-					})
-					.then(response=>response.json())
-					.then(data=>{ sessionStorage.setItem('streamData',JSON.stringify(data)); })
-
-
-				location.href="/show";
-						}
-	
-
-async function setRate(){
-await	fetch("/getRate").then(data=>data.json())
-	.then(data => {
-	//	console.log(data.ETH);
-	usdToEther= (data.ETH);
-});
+async function setVal(e) {
+    document.getElementById("cost").innerHTML = RATE*e.value;
+    document.getElementById("streamDur").innerHTML = e.value;
 }
 
+async function setRate() {
+    await	fetch("/getRate").then(data=>data.json())
+    .then(data => {
+        usdToEther = (data.ETH);
+    });
+}
 async function payFirst(e){
-
 	var name = document.getElementById("name").value;
 	if(!checkSpaces(name,false)){
 		alert("please enter a valid name");
 		return;
 	}
 
-	logs.style.display="block";
+	transaction.style.display="block";
 
 	await setRate();
 	var timeToPay = document.getElementById("duration").value;
 	var amount = RATE*timeToPay*usdToEther;
 	e.innerHTML="Please Wait..."
-	logs.innerHTML+=`<h3>Starting Payment sequence...</h3><p>Amount: ${amount} ether</p><p>Amount in USD: ${RATE*timeToPay} USD</p><p>Stream Name: </p>`;
-//	console.log(amount);
+	logs.innerHTML+=`<h3>Starting Payment sequence...</h3><p>Amount: ${amount} ether</p><p>Amount in USD: ${RATE*timeToPay} USD</p><p>Stream Name: <b>${name}</b></p>`;
 	initiateTxn(amount.toString(),e);	
 }
 
-
-
-function checkSpaces(str,exact) {
-	var len = str.replace(/\s/g, '').length
-	return (exact ? len===str.length && len!==0 : len!==0) 
+function checkSpaces(str, exact) {
+    var len = str.replace(/\s/g, '').length
+    return (exact ? len === str.length && len !== 0: len !== 0)
 }
 
-
-
+const scrollToBottom = (id) => {
+    const element = document.getElementById(id);
+    element.scrollTop = element.scrollHeight;
+}
